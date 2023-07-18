c5c95d014b4bdffe80f0ea75946a989652ce7883
MaycolChinito <barreramaycol028@gmail.com>
2023-07-17 21:38:39 -0600

    Agregar archivos de proyecto.

---
+++ SuperficieDelTrianguloprac15/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace SuperficieDelTrianguloprac15;
+
+//<Summary>
+//<Createddate>15/7/2023</createddate>
+//<company>INDEL</company>
+//<lastmodificationdate>16/7/2023</lastmodificationdate>
+//<lastmodificationdescription>
+// Construya un programa en MAUI que dadas la base y la altura de un triangulo, calcule e imprima la superficie 
+//</lastmodificationdescription>
+//<lastmodifierautor> Maycol Barrera </lastmodifierautor>
+//</Summary>
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        double AlturaTriangulo = Convert.ToDouble(Altura.Text);
+        double BaseTriangulo = Convert.ToDouble(basee.Text);
+
+
+        double superficie = BaseTriangulo * AlturaTriangulo /2;
+
+        SuperficieRespuesta.Text = Convert.ToString(superficie);
+
+    }
+}
+
